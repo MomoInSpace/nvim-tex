fcabb88c9ed9e0d508e1d2880c107d953276e2c4
m300912 <jacob.widmann@mpimet.mpg.de>
2022-10-06 15:33:39 +0200

    feat: Added very basic settings and colorscheme

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- GENERAL SETTINGS
+-- Dependencies: 
+
+--Very Basic nvim settings not using any plugins are in the basic folder:
+require("basic.basic_nvim_settings")
+require("basic.colorscheme")
+
