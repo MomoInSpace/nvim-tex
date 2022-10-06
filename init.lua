73d3f8ef06aa3b2e9acf2e87ffe23ebd6b48d46e
m300912 <jacob.widmann@mpimet.mpg.de>
2022-10-06 19:44:03 +0200

    fix: fixed relative pathing issue

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
 -- GENERAL SETTINGS
 -- Dependencies: 
 
+
 --Very Basic nvim settings not using any plugins are in the basic folder:
-require("/basic.basic_nvim_settings")
-require("/basic.colorscheme")
+require('basic/basic_nvim_settings')
+require('basic.colorscheme')
 
 -- Plugin Settings:
-require("/plugin.packer") -- This file installs packer automatically, if the file plugin/packer_compiled doesn't exist.
+require('plugin.packer') -- This file installs packer automatically, if the file plugin/packer_compiled doesn't exist.
---
+++ lua/basic/colorscheme.lua
@@ -0,0 +1,6 @@
+-- Set colorscheme (order is important here).
+vim.o.termguicolors = true
+vim.g.onedark_terminal_italics = 2
+vim.cmd [[colorscheme moonfly]]
+
+
